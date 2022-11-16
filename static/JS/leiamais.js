0f4e54a08365801936df9e44a21a6bcd3c6961ca
projeto.fgmp <projeto.fgmp@gmail.com>
2022-11-16 19:20:12 -0300

    page analise

--- static/JS/leiamais.js
+++ static/JS/leiamais.js
@@ -7,11 +7,11 @@ function leiaMais(){
     if(pontos.style.display === "none" ){
         pontos.style.display="inline";
         maisTexto.style.display="none";
-        btnLeiaMais.innerHTML="Veja Mais - Artilheiros";
+        btnLeiaMais.innerHTML="Veja Mais";
     }else{
         pontos.style.display="none";
         maisTexto.style.display="inline";
-        btnLeiaMais.innerHTML="Veja Menos - Artilheiros";
+        btnLeiaMais.innerHTML="Veja Menos";
     }
 }
 
@@ -28,18 +28,166 @@ function leiaMais2(){
     if(pontos.style.display === "none" ){
         pontos.style.display="inline";
         maisTexto.style.display="none";
-        btnLeiaMais.innerHTML="Veja Mais - Média Gols";
+        btnLeiaMais.innerHTML="Veja Mais";
     }else{
         pontos.style.display="none";
         maisTexto.style.display="inline";
-        btnLeiaMais.innerHTML="Veja Menos - Média Gols";
+        btnLeiaMais.innerHTML="Veja Menos";
     }
 }
 
 var slides = document.querySelectorAll('.slide');
     var btns = document.querySelectorAll('.btn');
-    let currentSlide2 = 1;    
+    let currentSlide2 = 1;  
+    
+    
+    /* LEIA MAIS 3 */
+function leiaMais3(){
+    var pontos=document.getElementsByClassName("pontos")[2];
+    var maisTexto=document.getElementsByClassName("mais")[2];
+    var btnLeiaMais=document.getElementsByClassName("btnLeiaMais")[2];
 
+    if(pontos.style.display === "none" ){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnLeiaMais.innerHTML="Veja Mais";
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="Veja Menos";
+    }
+}
+
+var slides = document.querySelectorAll('.slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide3 = 1;  
+
+
+     /* LEIA MAIS 4 */
+function leiaMais4(){
+    var pontos=document.getElementsByClassName("pontos")[3];
+    var maisTexto=document.getElementsByClassName("mais")[3];
+    var btnLeiaMais=document.getElementsByClassName("btnLeiaMais")[3];
+
+    if(pontos.style.display === "none" ){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnLeiaMais.innerHTML="Veja Mais";
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="Veja Menos";
+    }
+}
+
+var slides = document.querySelectorAll('.slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide4 = 1;  
+
+
+     /* LEIA MAIS 5 */
+     function leiaMais5(){
+        var pontos=document.getElementsByClassName("pontos")[4];
+        var maisTexto=document.getElementsByClassName("mais")[4];
+        var btnLeiaMais=document.getElementsByClassName("btnLeiaMais")[4];
+    
+        if(pontos.style.display === "none" ){
+            pontos.style.display="inline";
+            maisTexto.style.display="none";
+            btnLeiaMais.innerHTML="Veja Mais";
+        }else{
+            pontos.style.display="none";
+            maisTexto.style.display="inline";
+            btnLeiaMais.innerHTML="Veja Menos";
+        }
+    }
+    
+    var slides = document.querySelectorAll('.slide');
+        var btns = document.querySelectorAll('.btn');
+        let currentSlide5 = 1;  
+    
+        /* LEIA MAIS 6 */
+    function leiaMais6(){
+        var pontos=document.getElementsByClassName("pontos")[5];
+        var maisTexto=document.getElementsByClassName("mais")[5];
+        var btnLeiaMais=document.getElementsByClassName("btnLeiaMais")[5];
+    
+        if(pontos.style.display === "none" ){
+            pontos.style.display="inline";
+            maisTexto.style.display="none";
+            btnLeiaMais.innerHTML="Veja Mais";
+        }else{
+            pontos.style.display="none";
+            maisTexto.style.display="inline";
+            btnLeiaMais.innerHTML="Veja Menos";
+        }
+    }
+    
+    var slides = document.querySelectorAll('.slide');
+        var btns = document.querySelectorAll('.btn');
+        let currentSlide6 = 1;  
+
+        /* LEIA MAIS 7 */
+    function leiaMais7(){
+        var pontos=document.getElementsByClassName("pontos")[6];
+        var maisTexto=document.getElementsByClassName("mais")[6];
+        var btnLeiaMais=document.getElementsByClassName("btnLeiaMais")[6];
+    
+        if(pontos.style.display === "none" ){
+            pontos.style.display="inline";
+            maisTexto.style.display="none";
+            btnLeiaMais.innerHTML="Veja Mais";
+        }else{
+            pontos.style.display="none";
+            maisTexto.style.display="inline";
+            btnLeiaMais.innerHTML="Veja Menos";
+        }
+    }
+    
+    var slides = document.querySelectorAll('.slide');
+        var btns = document.querySelectorAll('.btn');
+        let currentSlide7 = 1;  
     
 
+       /* LEIA MAIS 8 */
+       function leiaMais8(){
+        var pontos=document.getElementsByClassName("pontos")[7];
+        var maisTexto=document.getElementsByClassName("mais")[7];
+        var btnLeiaMais=document.getElementsByClassName("btnLeiaMais")[7];
+    
+        if(pontos.style.display === "none" ){
+            pontos.style.display="inline";
+            maisTexto.style.display="none";
+            btnLeiaMais.innerHTML="Veja Mais";
+        }else{
+            pontos.style.display="none";
+            maisTexto.style.display="inline";
+            btnLeiaMais.innerHTML="Veja Menos";
+        }
+    }
+    
+    var slides = document.querySelectorAll('.slide');
+        var btns = document.querySelectorAll('.btn');
+        let currentSlide8 = 1;  
+
+        /* LEIA MAIS 9 */
+       function leiaMais9(){
+        var pontos=document.getElementsByClassName("pontos")[8];
+        var maisTexto=document.getElementsByClassName("mais")[8];
+        var btnLeiaMais=document.getElementsByClassName("btnLeiaMais")[8];
+    
+        if(pontos.style.display === "none" ){
+            pontos.style.display="inline";
+            maisTexto.style.display="none";
+            btnLeiaMais.innerHTML="Veja Mais";
+        }else{
+            pontos.style.display="none";
+            maisTexto.style.display="inline";
+            btnLeiaMais.innerHTML="Veja Menos";
+        }
+    }
+    
+    var slides = document.querySelectorAll('.slide');
+        var btns = document.querySelectorAll('.btn');
+        let currentSlide9 = 1;  
     
